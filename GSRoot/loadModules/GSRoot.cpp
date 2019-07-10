1f93c20226ad891d6239e0813a9667f941035bb2
HaoRui <hr854164641@qq.com>
2019-07-10 16:30:25 +0800

    添加GSRoot工程

---
+++ GSRoot/loadModules/GSRoot.cpp
@@ -0,0 +1,54 @@
+﻿// GSRoot.cpp : 定义 DLL 应用程序的导出函数。
+#include "../stdafx.h"
+
+#define TEST 0;
+
+#if TEST == 1
+class Pet {
+public:
+	Pet() {}
+	Pet(const std::string &name) { this->name = name; }
+
+	void setName(const std::string &name_) { name = name_; }
+	const std::string &getName() const { return name; }
+
+	int add(int a = 1, int b = 5) { return a + b; }
+
+	std::string name;
+};
+
+void load_test(py::module m) {
+	py::class_<Pet> m_pet(m, "Pet");
+	m_pet
+		.def(py::init<>())
+		.def(py::init<const std::string &>())
+
+		.def("setName", &Pet::setName)
+		.def("getName", &Pet::getName)
+		.def("add", &Pet::add, py::arg("a"), py::arg("b") = 5)
+
+		.def("__str__", [](const Pet &p) {
+		return "name is " + p.name;
+	});
+}
+#endif // TEST == 1
+
+
+
+
+
+PYBIND11_MODULE(GSRoot, m) {
+
+	m.doc() = "version 2019.7.10";
+
+	// Add bindings here
+	m.def("foo", []() {
+		return "Hello, World! GSRoot";
+	});
+
+#if  TEST == 1
+	load_test(m);
+#else
+
+#endif //  TEST == 1
+}
