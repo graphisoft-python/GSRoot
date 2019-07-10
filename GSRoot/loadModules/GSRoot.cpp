471d257ca8242c2946058197aa4882c61c8a39ae
HaoRui <hr854164641@qq.com>
2019-07-10 20:43:48 +0800

    1、合并主分支代码

--- GSRoot/loadModules/GSRoot.cpp
+++ GSRoot/loadModules/GSRoot.cpp
@@ -1,6 +1,8 @@
 ﻿// GSRoot.cpp : 定义 DLL 应用程序的导出函数。
 #include "../stdafx.h"
 
+#include "../implementations/gsGuidFunc.hpp"
+
 #define TEST 0;
 
 #if TEST == 1
@@ -49,6 +51,6 @@ PYBIND11_MODULE(GSRoot, m) {
 #if  TEST == 1
 	load_test(m);
 #else
-
+	load_gs_Guid(m);
 #endif //  TEST == 1
 }
