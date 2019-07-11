f1aee91cbb9b2a20609ac50593f8a4a153930474
HaoRui <hr854164641@qq.com>
2019-07-11 16:46:45 +0800

    1、完成GSRoot工程的移植，且结合DGLib在archicad上调试通过

--- GSRoot/implementations/gsGuidFunc.hpp
+++ GSRoot/implementations/gsGuidFunc.hpp
@@ -1,5 +1,8 @@
 #include "../stdafx.h"
+
 #include "GSGuid.hpp"
+
+#include "ACCast.h"
 
 using namespace GS;
 
@@ -7,22 +10,20 @@ void load_gs_Guid(py::module m) {
 	py::class_<Guid>(m, "Guid")
 		.def(py::init<>())
 		.def(py::init<char *>())
+		.def(py::init<UniString &>())
+		//.def(py::self = py::self)
 		
 		.def("Generate", [](Guid &self) { 
-			return self.Generate() == NoError; 
+			return self.Generate() == NoError;
 		})
 		.def("Clear", &Guid::Clear)
-		.def("ConvertToString", [](Guid &self) {
-			char buffer[GUIDStringLength];
-			if (self.ConvertToString(buffer) == NoError) {
-				return std::string(buffer);
-			}else {
-				return std::string("");
-			}
-			//GS::UniString -> unicode  
+		.def("ToUniString", &Guid::ToUniString)
+		.def("ConvertFromString", [](Guid &self,const UniString &string) {
+			return self.ConvertFromString(string) == NoError;
 		})
-
-
+		.def("ConvertFromPrefixString", [](Guid &self, const UniString &string) {
+			return self.ConvertFromPrefixString(string) == NoError;
+		})
 		.def("Invert", &Guid::Invert)
 		.def("IsEqual", &Guid::IsEqual)
 		.def("IsNull", &Guid::IsNull)
