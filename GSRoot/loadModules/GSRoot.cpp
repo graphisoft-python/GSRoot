ca74137a0c38e13b915c2ab7ecd47c4e10be5885
WangJun <445212619@qq.com>
2019-07-25 14:08:16 +0800

    1.移除无效代码；2.改进Guid类

--- GSRoot/loadModules/GSRoot.cpp
+++ GSRoot/loadModules/GSRoot.cpp
@@ -9,11 +9,8 @@ PYBIND11_MODULE(GSRoot, m) {
 
 	m.def("version", []() {	return "version 0.0.2"; });
 
-	// --- Add bindings here ------------------------------------------------------------------
+// --- Add bindings here ------------------------------------------------------------------
 
-	m.def("foo", []() {
-		return "Hello, World! GSRoot";
-	});
 
 // --- GSGuid
 	load_gs_Guid(m);
@@ -21,5 +18,5 @@ PYBIND11_MODULE(GSRoot, m) {
 // --- Color
 	load_gs_Color(m);
 
-	// --- Add bindings end -------------------------------------------------------------------
+// --- Add bindings end -------------------------------------------------------------------
 }
